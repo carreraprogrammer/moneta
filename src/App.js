a33e9822adad73eaa8e903851b2e8465edcc2e58
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-12-18 19:45:37 -0500

    Fix display bug

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
  import React from "react";
  import { Route, Routes } from 'react-router-dom';
- import { Provider } from 'react-redux';
  import Home from "./pages/Home";
  import About from "./pages/About";
  import Shop from "./pages/Shop";
