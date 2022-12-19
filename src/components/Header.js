fa930c6e8dd3f68cd4351b04e3e8f54c5ba804ff
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-12-19 00:30:53 -0500

    first version parallax effect

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
+import front from '../resources/Images/parallaxOne.svg'
 
 const Header = () => {
   return(
     <header className='header'>
       <h1 id='headerTitle'>MONETA</h1>
+      <img id='frontImage' src={front} />
     </header>
   )
 }
