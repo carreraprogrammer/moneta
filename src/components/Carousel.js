70d5152ea71ad2bc93e93a5ced9afb64fd8c82e0
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-12-22 09:52:29 -0500

    Update carousel animation

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -11,41 +11,41 @@ const Carousel = () => {
   
   const stopCarousel = () => {
       $('#carouselOne > :nth-child(1)').css({
-          transform: 'translateX(0)',
+          transform: 'translateX(-3%)',
           transition: 'all 20s ease-in-out'
         })
       $('#carouselOne > :nth-child(3)').css({
-          transform: 'translateX(0)',
+          transform: 'translateX(-3%)',
           transition: 'all 20s ease-in-out'
         })
       $('#carouselOne > :nth-child(2)').css({
-          transform: 'translateX(0)',
+          transform: 'translateX(-9%)',
           transition: 'all 20s ease-in-out'
         })
     }
 
     const startCarousel = () => {
         $('#carouselOne > :nth-child(1)').css({
-          transform: 'translateX(-8%)',
+          transform: 'translateX(8%)',
           transition: 'all 5s ease-in-out'
         })
         $('#carouselOne > :nth-child(3)').css({
-          transform: 'translateX(-8%)',
+          transform: 'translateX(8%)',
           transition: 'all 5s ease-in-out'
         })
         $('#carouselOne > :nth-child(2)').css({
-          transform: 'translateX(-32%)',
+          transform: 'translateX(32%)',
           transition: 'all 5s ease-in-out'
         })
       }
   
   return (
-  <div id='carouselOne' onMouseLeave={startCarousel} onMouseOver={stopCarousel} data-aos='fade-right' data-aos-anchor-placement="center-bottom">
-    <img className='carouselOneImg' src={carousel} alt='carousel'/>
-    <div id='carouselBtnContainer'>
+  <div id='carouselOne' onMouseLeave={startCarousel} onMouseOver={stopCarousel}>
+    <img className='carouselOneImg' src={carousel} alt='carousel'  data-aos-easing="ease-in-sine" data-aos='carouselRight' data-aos-anchor-placement="top-bottom" data-aos-duration='2000' data-aos-anchor='#homeGallery' />
+    <div id='carouselBtnContainer' data-aos-easing="ease-in-sine" data-aos='carouselBtnRight' data-aos-anchor-placement="top-center" data-aos-duration='2000'>
       <button id='carouselBtn' style={{marginRight: '10px', marginBottom: '10px',}}>Menú</button>
     </div>
-    <img className='carouselOneImg' src={carousel} alt='carousel'/>
+    <img className='carouselOneImg' src={carousel} alt='carousel'  data-aos-easing="ease-in-sine" data-aos='carouselRight' data-aos-anchor-placement="top-bottom" data-aos-duration='2000'/>
   </div>
   )
 }
