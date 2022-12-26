368fb18b07c54de26b5f8bae63fbf260ab4bb01b
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-12-26 13:36:02 -0500

    solving svg size

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React, {useEffect} from 'react'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
-import frontImage from '../resources/Images/frontPage.png'
+import frontImage from '../resources/Images/newFrontPage.svg'
 
 const Header = () => {
   useEffect(() => {
