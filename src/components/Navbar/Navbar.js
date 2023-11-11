b581f823d68b297d3043fa66341503e1c415b590
Daniel <carreraprogrammer@gmail.com>
2023-11-11 16:23:46 -0500

    Refactor Navbar component with gsap and ScrollTrigger

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,65 +1,46 @@
 import { NavLink } from 'react-router-dom';
-import $ from 'jquery'
-import { useEffect, useState } from 'react';
-import './Navbar.scss'
+import { useEffect, useRef, useState } from 'react';
+import './Navbar.scss';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Navbar = () => {
-  const [prevScrollPos, setPrevScrollPos] = useState(0);
-
+  const navbarRef = useRef(null);
+  const [navActive, setNavActive] = useState(false);
 
   const handleNavigation = () => {
-    $('.navigation').toggleClass('navigationActive')
-    $('.burgerLine').toggleClass('burgerActive')
-  }
-  
+    setNavActive(!navActive);
+  };
+
   useEffect(() => {
-    const handleScroll = () => {
-      // Get the current scroll position
-      const currentScrollPos = window.pageYOffset;
-
-      const navbar = document.querySelector('.nav')
-
-      // If the user is scrolling down and the navbar is not already hidden
-      if (currentScrollPos  > prevScrollPos && !navbar.classList.contains('navhidde')) {
-        // Add the 'navhidde' class to hide the navbar
-        navbar.classList.add('navhidde');
+    gsap.to(navbarRef.current, {
+      scrollTrigger: {
+        trigger: navbarRef.current,
+        start: "top top",
+        end: "bottom top",
+        toggleClass: { targets: navbarRef.current, className: "navhidde" },
+        scrub: true
       }
-      // If the user is scrolling up and the navbar is already hidden
-      else if (currentScrollPos <= prevScrollPos  && navbar.classList.contains('navhidde')) {
-        // Remove the 'navhidde' class to display the navbar
-        navbar.classList.remove('navhidde');
-      }
-
-      // Update the previous scroll position
-      setPrevScrollPos(currentScrollPos);
-    };
-
-    // Add an event listener for the scroll event
-    window.addEventListener('scroll', handleScroll);
-
-    // Remove the event listener when the component is unmounted
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-
-  }, [prevScrollPos]);
+    });
+  }, []);
   
   return(
-    <nav className='nav'>
-      <ul className='navigation'>
+    <nav ref={navbarRef} className={`nav ${navActive ? 'navActive' : ''}`}>
+      <ul className={`navigation ${navActive ? 'navigationActive' : ''}`}>
         <NavLink className='navigationLinks' to='/' onClick={handleNavigation}>Principal</NavLink>
         <NavLink className='navigationLinks' to='/Coffee' onClick={handleNavigation}>Café</NavLink>
         <NavLink className='navigationLinks' to='/Shop' onClick={handleNavigation}>Tienda</NavLink>
         <NavLink className='navigationLinks' to='/About' onClick={handleNavigation}>About</NavLink>
       </ul>
       <div className='burgerMenu' onClick={handleNavigation}>
-        <span className='burgerLine'></span>    
-        <span className='burgerLine'></span>
-        <span className='burgerLine'></span>
+        <span className={`burgerLine ${navActive ? 'burgerActive' : ''}`}></span>    
+        <span className={`burgerLine ${navActive ? 'burgerActive' : ''}`}></span>
+        <span className={`burgerLine ${navActive ? 'burgerActive' : ''}`}></span>
       </div>
     </nav>
   )
 }
 
-
-export default Navbar 
+export default Navbar;
