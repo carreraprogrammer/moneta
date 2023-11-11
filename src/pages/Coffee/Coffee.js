6d7d68984e438f51554d403a7fbcc40a91a81f49
Daniel <carreraprogrammer@gmail.com>
2023-11-11 16:23:52 -0500

    Refactor Coffee.js file

--- src/pages/Coffee/Coffee.js
+++ src/pages/Coffee/Coffee.js
@@ -1,15 +1,8 @@
-import AOS from 'aos'
-import { useEffect } from 'react'
 import sandwich from '../../assets/Images/SandwichWithBrew.png'
 import Menu from '../../components/Menu/Menu'
 import './Coffee.scss'
 
 const Coffee = () => {
-  useEffect(() => {
-    AOS.init({duration: 2000});
-    window.scrollTo(0, 0);
-  }, [])
-
 
   return(
     <>
@@ -18,7 +11,7 @@ const Coffee = () => {
         <p id='coffeeHeaderDescription'>Buscamos ser un gran lugar de memoria, donde podamos compartir el amor por el café y la gastronomia nariñense; intercambiar saberes y ser semillas de sueños nuevos</p>
       </div>
     <div id='homeGallery' style={{background: '#ea984e'}}>
-      <div id='galleryContainer' data-aos='fade-in' data-aos-anchor-place='center-center' data-aos-easing="ease-in-sine"  data-aos-duration='2000' style={{border: '4px solid #6d0022', width: '90%'}}>
+      <div id='galleryContainer'>
         <div id='homeGalleryOne' style={{background: `url(${sandwich})`, backgroundSize: 'cover', flexBasis: '100%', backgroundPositionX: 'center', backgroundPositionY: 'center'}}></div>
       </div>
     </div>
