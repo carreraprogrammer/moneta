c2323e82568a0fd3b748f7371ac97ad6d82b4ee2
Daniel <carreraprogrammer@gmail.com>
2023-11-11 16:22:57 -0500

    Add declarations for png and svg files.

---
+++ declarations.d.ts
@@ -0,0 +1,9 @@
+// declaration.d.ts
+
+declare module '*.png';
+declare module '*.svg' {
+  import React = require('react');
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
