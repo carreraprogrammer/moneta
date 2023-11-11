a4d88b0ba94db355111d7b42244c651fb2970f53
Daniel <carreraprogrammer@gmail.com>
2023-11-11 17:02:39 -0500

    Refactor Gallery component to use TypeScript and updated animations

---
+++ src/components/Gallery/Gallery.tsx
@@ -0,0 +1,22 @@
+import React, { useRef } from 'react';
+import './Gallery.scss';
+import useScrollTriggerAnimations from '../../customHooks/useScrollTriggerAnimations';
+import { fadeInAnimation } from '../../animations/animations';
+
+const Gallery: React.FC = () => {
+  const galleryContainer = useRef<HTMLDivElement | null>(null);
+
+  useScrollTriggerAnimations(galleryContainer, [fadeInAnimation]);
+
+  return (
+    <div id='homeGallery'>
+      <div id='galleryContainer' ref={galleryContainer}>
+        <div id='homeGalleryOne'></div>
+        <div id='homeGalleryTwo'></div>
+        <div id='homeGalleryThree'></div>
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
