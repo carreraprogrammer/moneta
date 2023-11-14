fd3e0a354e9540cefd0440af39b26723276b26c7
Daniel <carreraprogrammer@gmail.com>
2023-11-14 16:29:46 -0500

    Update import path for Events component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -13,7 +13,7 @@ import Gallery from "../../components/Gallery/Gallery";
 import Carousel from "../../components/Carousel/Carousel";
 import AboutAbstract from "../../section/AboutAbstract/AboutAbstract";
 import ShopHome from "../../section/ShopHome/ShopHome";
-import Events from "../../section/Events/Events";
+import Events from "../../section/Events/Blogs";
 import Footer from "../../section/Footer/Footer";
 
 const Home = () => {
