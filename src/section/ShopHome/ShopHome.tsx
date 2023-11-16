64eb016d8d09b4406ea374bfd5ceb3e65951acb3
Daniel <carreraprogrammer@gmail.com>
2023-11-15 22:20:04 -0500

    Update shop title to uppercase

--- src/section/ShopHome/ShopHome.tsx
+++ src/section/ShopHome/ShopHome.tsx
@@ -21,7 +21,7 @@ const ShopHome: React.FC = () => {
 
   return (
     <div id='shopSection'>
-      <h1 id='shopTitle' ref={titleRef}>Nuestra Tienda</h1>
+      <h2 id='shopTitle' ref={titleRef}>NUESTRA TIENDA</h2>
       <p id='shopDescription' ref={descriptionRef}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit ipsum dolor sit amet, consectetur adipiscing elit. ipsum dolor sit amet, consectetur adipiscing elit ipsum dolor sit amet, consectetur adipiscing elit..
       </p>
