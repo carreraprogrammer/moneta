8c1d8e90114431277f0489948cfee7d06e9c1ae6
Daniel <carreraprogrammer@gmail.com>
2023-11-16 19:04:28 -0500

    Remove home gallery section

--- src/pages/Coffee/Coffee.js
+++ src/pages/Coffee/Coffee.js
@@ -10,13 +10,11 @@ const Coffee = () => {
         <h1 id='coffeeHeaderTitle'>Café</h1>
         <p id='coffeeHeaderDescription'>Buscamos ser un gran lugar de memoria, donde podamos compartir el amor por el café y la gastronomia nariñense; intercambiar saberes y ser semillas de sueños nuevos</p>
       </div>
-    <div id='homeGallery'>
       <div id='coffeeGalleryContainer'>
         <div id='coffeeGalleryOne'>
           <img src={sandwich} alt='sandwich' />
         </div>
       </div>
-    </div>
     <Menu />
     </>
   )
