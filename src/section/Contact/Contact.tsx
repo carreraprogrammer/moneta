84b9a140e01083b684eae7886a8036a54b04dea3
Daniel <carreraprogrammer@gmail.com>
2023-11-17 18:05:57 -0500

    Update Contact form styling and add Button component

--- src/section/Contact/Contact.tsx
+++ src/section/Contact/Contact.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import './Contact.scss'
+import Button from '../../components/Button/Button'
 
 const Contact:React.FC = () => {
   return (
     <section id='contactSection'>
       <h2 id='contactTitle'>CONTACTANOS</h2>
       <form>
-        <input type='text' placeholder='Nombre' />
-        <input type='text' placeholder='Email' />
-        <input type='text' placeholder='Telefono' />
+        <input type='text' className= 'formInputs' placeholder='Nombre' />
+        <input type='text' className= 'formInputs' placeholder='Email' />
+        <input type='text' className= 'formInputs' placeholder='Telefono' />
         <textarea placeholder='Mensaje' />
+        <Button />
       </form>
     </section>
   )
