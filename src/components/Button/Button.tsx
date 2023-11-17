606f2c24af12da9b5bcb417bdb3c851081adb8cf
Daniel <carreraprogrammer@gmail.com>
2023-11-17 18:05:42 -0500

    Add Button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import './Button.scss'
+
+import React from 'react'
+
+const Button = () => {
+  return (
+    <input type='button' className='button' value='Enviar'/>
+  )
+}
+
+export default Button
