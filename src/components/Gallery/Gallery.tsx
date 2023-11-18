c351f4b1af7d1994e47c5c7404c5763c1c6ae716
Daniel <carreraprogrammer@gmail.com>
2023-11-18 18:41:10 -0500

    Add description to home gallery

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -11,12 +11,12 @@ const Gallery: React.FC = () => {
   return (
     <div id='homeGallery'>
       <h2 id='homeGalleryTitle'>CAFE Y MEMORIA</h2>
+      <p className='homeGalleryDescription'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos quam culpa facilis et enim expedita molestias neque, ipsam optio nulla, nam eveniet id vero iure reiciendis sit a cumque molestiae! </p>
       <div id='galleryContainer' ref={galleryContainer}>
         <div id='homeGalleryOne'></div>
         <div id='homeGalleryTwo'></div>
         <div id='homeGalleryThree'></div>
       </div>
-      <p className='homeGalleryDescription'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos quam culpa facilis et enim expedita molestias neque, ipsam optio nulla, nam eveniet id vero iure reiciendis sit a cumque molestiae! </p>
     </div>
   );
 };
