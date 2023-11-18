371b5bec9c4ad818b7c68c05c4b22594c9b4f572
Daniel <carreraprogrammer@gmail.com>
2023-11-18 18:41:19 -0500

    Add MenuCard component

---
+++ src/components/MenuCard/MenuCard.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Category } from '../../assets/Information/Menu';
+
+const MenuCard: React.FC<Category> = ({ category }) => {
+  return (
+    <section className='menuCard'>
+      {category.subCategories.map((subCategory, subIndex) => (
+        <div key={subIndex}>
+          <h2 className='menuCardTitle'>{subCategory.name}</h2>
+          <div className='menuCardItems'>
+            {subCategory.products.map((item, index) => (
+              <div className='menuItem' key={index}>
+                <h3 className='menuItemTitle'>{item.name}</h3>
+                <p className='menuItemDescription'>{item.productInfo}</p>
+                <p className='menuItemPrice'>{item.price}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </section>
+  );
+}
+
+export default MenuCard;
