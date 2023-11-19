38a545e55e77a9cc3af8467fa80eb064f78f18f1
Daniel <carreraprogrammer@gmail.com>
2023-11-19 15:09:50 -0500

    Refactor MenuCard component to use updated class names

--- src/components/MenuCard/MenuCard.tsx
+++ src/components/MenuCard/MenuCard.tsx
@@ -7,16 +7,15 @@ interface MenuCardProps {
 
 const MenuCard: React.FC<MenuCardProps> = ({ category }) => {
   return (
-    <section className='menuCard'>
+    <section className='menuSection'>
       {category.subCategories.map((subCategory, subIndex) => (
-        <div key={subIndex}>
+        <div key={subIndex} className='menuCard'>
           <h2 className='menuCardTitle'>{subCategory.name}</h2>
           <div className='menuCardItems'>
             {subCategory.products.map((item, index) => (
               <div className='menuItem' key={index}>
                 <h3 className='menuItemTitle'>{item.name}</h3>
-                <p className='menuItemDescription'>{item.productInfo}</p>
-                <p className='menuItemPrice'>{item.price}</p>
+                <p className='menuItemDescription'><span>{item.productInfo}</span><span>${item.price}</span></p>
               </div>
             ))}
           </div>
