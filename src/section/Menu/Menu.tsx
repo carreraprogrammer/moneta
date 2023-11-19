b1cf329705728d2d16967304b88efdde34d37f43
Daniel <carreraprogrammer@gmail.com>
2023-11-19 16:16:00 -0500

    Refactor menu filtering logic

--- src/section/Menu/Menu.tsx
+++ src/section/Menu/Menu.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import menu from '../../assets/Information/Menu';
 import './Menu.scss';
 import MenuCard from '../../components/MenuCard/MenuCard';
@@ -7,54 +7,60 @@ import { MenuTypo, Category } from '../../assets/Information/Menu';
 type MenuKey = keyof MenuTypo;
 
 const Menu: React.FC = () => {
-
-  const [menuData, setMenuData] = React.useState<MenuTypo>(menu);
+  const [menuData, setMenuData] = useState<MenuTypo>(menu);
 
   const filterMenu = (category: string) => {
-    if (category === 'Todos') {
-      setMenuData(menu);
-    } else if (category in menu) {
-      setMenuData(()=> { return { [category]: menu[category as MenuKey]}});
-    }
-    console.log(category);
-    console.log(menuData);
-  }
+    const newMenuData = category === 'Todos' ? menu : { [category]: menu[category as MenuKey] };
+    setMenuData(newMenuData);
+  };
 
   const filterMenuSubcategory = (subcategory: string) => {
-    const category = Object.keys(menuData)[0]
-    const subCategory = (menuData[category as MenuKey] as Category).subCategories.filter((subCategory) => subCategory.name === subcategory)[0]
+    const category = Object.keys(menuData)[0];
+    const subCategoryData = (menuData[category as MenuKey] as Category).subCategories.find(sc => sc.name === subcategory);
     
-    setMenuData(()=> { return { [category]: { subCategories: [subCategory] } }});
-  }
+    if (subCategoryData) {
+      setMenuData({ [category]: { subCategories: [subCategoryData] } });
+    }
+  };
+
+  const renderMenuFilters = () => (
+    <div id='menuFilters'>
+      <button className='menuFilter' onClick={() => filterMenu('Todos')}>Todos</button>
+      {Object.keys(menu).map(key => (
+        <button key={key} className='menuFilter' onClick={() => filterMenu(key)}>{key}</button>
+      ))}
+    </div>
+  );
+
+  const renderMenuSubcategoryFilters = () => {
+    if (Object.keys(menuData).length === 1) {
+      return (
+        <div className='menuSubFilters'>
+          {(menuData[Object.keys(menuData)[0] as MenuKey] as Category).subCategories.map((subCategory, index) => (
+            <button key={index} className='menuSubFilter' onClick={() => filterMenuSubcategory(subCategory.name)}>{subCategory.name}</button>
+          ))}
+        </div>
+      );
+    }
+    return null;
+  };
+
+  const renderMenuSections = () => (
+    Object.keys(menuData).map(key => (
+      <div key={key} className='sectionContainer'>
+        <h2 className='sectionTitle'>{key}</h2>
+        <MenuCard category={menuData[key as MenuKey] as Category} />
+      </div>
+    ))
+  );
 
   return (
     <div id='menu'>
       <h1 id='menuTitle'>MENÚ</h1>
-      <div id='menuFilters'>
-          <button className='menuFilter' onClick={() => filterMenu('Todos')}>Todos</button>
-        {Object.keys(menu).map((key)=> (
-          <button key={key} className='menuFilter' onClick={() => filterMenu(key)}>{key}</button>
-        ))}
-      </div>
-      <div id='menuSubcategoryFilters'>
-        { Object.keys(menuData).length === 1 ? 
-          <div className='menuSubFilters'>
-            {Object.keys(menuData).map((key)=> (
-              (menuData[key as MenuKey] as Category).subCategories.map((subCategory, index) => (
-                <button key={index} className='menuSubFilter' onClick={() => filterMenuSubcategory(subCategory.name)}>{subCategory.name}</button>
-              ))
-            ))}
-          </div>
-        : null }
-      </div>
+      {renderMenuFilters()}
+      {renderMenuSubcategoryFilters()}
       <div id='menuContainer'>
-        {Object.keys(menuData).map((key)=> (
-          <div key={key} className='sectionContainer'>
-            <h2 className='sectionTitle'>{key}</h2>
-            <MenuCard category={menuData[key as MenuKey] as Category} />
-          </div>
-        )
-        )}
+        {renderMenuSections()}
       </div>
     </div>
   );
