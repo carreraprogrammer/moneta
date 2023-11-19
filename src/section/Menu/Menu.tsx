8d78733c1dae44cd4d3734e4a26328995d389385
Daniel <carreraprogrammer@gmail.com>
2023-11-19 10:14:01 -0500

    Refactor Menu component to use MenuCard component

--- src/section/Menu/Menu.tsx
+++ src/section/Menu/Menu.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import menu from '../../assets/Information/Menu';
 import './Menu.scss';
-import { MenuItem } from '../../assets/Information/Menu';
+import MenuCard from '../../components/MenuCard/MenuCard';
+import { MenuTypo } from '../../assets/Information/Menu';
+
+type MenuKey = keyof MenuTypo;
 
 const Menu: React.FC = () => {
-  const tintos: MenuItem[] = menu.filter((section: MenuItem) => section.subCategory === 'Tintos');
-
   return (
     <div id='menu'>
       <h1 id='menuTitle'>MENÚ</h1>
+      {Object.keys(menu).map((key)=> (
+        <div key={key} className='sectionContainer'>
+          <h2 className='sectionTitle'>{key}</h2>
+          <MenuCard category={menu[key as MenuKey]} />
+        </div>
+      )
+      )}
     </div>
   );
 };
