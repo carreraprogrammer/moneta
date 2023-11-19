9fdeea3d7a0cc10a4368c440ad02c9dc1a864a48
Daniel <carreraprogrammer@gmail.com>
2023-11-19 16:48:10 -0500

    Add ShoppingListButton component

--- src/pages/Coffee/Coffee.js
+++ src/pages/Coffee/Coffee.js
@@ -3,11 +3,13 @@ import Menu from '../../section/Menu/Menu'
 import './Coffee.scss'
 import Contact from '../../section/Contact/Contact'
 import Footer from '../../section/Footer/Footer'
+import ShoppingListButton from '../../components/ShoppingListButton/ShoppingListButton'
 
 const Coffee = () => {
 
   return(
     <>
+      <ShoppingListButton />
       <div id='coffeeHeader'>
         <h1 id='coffeeHeaderTitle'>Café</h1>
         <p id='coffeeHeaderDescription'>Buscamos ser un gran lugar de memoria, donde podamos compartir el amor por el café y la gastronomia nariñense; intercambiar saberes y ser semillas de sueños nuevos</p>
