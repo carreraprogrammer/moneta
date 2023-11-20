99f504256c9df8dc2554b92bcb6a7fd1536858b2
Daniel <carreraprogrammer@gmail.com>
2023-11-20 10:15:46 -0500

    Add new sections to Shop page

--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import ShopHeader from '../../section/ShopHeader/ShopHeader'
 import FeaturedProducts from '../../section/FeaturedProducts/FeaturedProducts'
+import ProductsGrid from '../../section/ProductsGrid/ProductsGrid'
+import Contact from '../../section/Contact/Contact'
+import Footer from '../../section/Footer/Footer'
 
 const Shop:React.FC = () => {
   return (
     <div>
       <ShopHeader />
       <FeaturedProducts />
+      <ProductsGrid />
+      <Contact />
+      <Footer />
     </div>
   )
 }
