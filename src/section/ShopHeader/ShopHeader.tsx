59319471f374cd7c41efb229aa7ddada54624dca
Daniel <carreraprogrammer@gmail.com>
2023-11-19 19:35:01 -0500

    Add ShopHeader component

---
+++ src/section/ShopHeader/ShopHeader.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './ShopHeader.scss'
+
+const ShopHeader = () => {
+  return (
+    <div className="shopHeader">
+      <h1 className="shopHeaderTitle">Shop</h1>
+      <p className="shopHeaderDescription">
+        Descubre nuestra cuidada selección de productos de café, desde granos selectos hasta accesorios de alta calidad. En nuestra tienda, cada artículo ha sido elegido pensando en los amantes del café, comprometidos con la sostenibilidad y la excelencia, te invitamos a 
+        explorar y disfrutar de lo mejor del mundo este mundo. Haz de cada mañana una experiencia excepcional.
+      </p>
+    </div>
+  )
+}
+
+export default ShopHeader
