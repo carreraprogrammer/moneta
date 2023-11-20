34bc909d253ac1d3747eca66a0d936657dd6263b
Daniel <carreraprogrammer@gmail.com>
2023-11-20 10:15:54 -0500

    Add featured products title

--- src/section/FeaturedProducts/FeaturedProducts.tsx
+++ src/section/FeaturedProducts/FeaturedProducts.tsx
@@ -5,6 +5,7 @@ import GallerySlider from '../../components/GallerySlider/GallerySlider'
 const FeaturedProducts = () => {
   return (
     <div className='featuredProductsContainer'>
+      <h2 className='featuredProductsTitle'>PRODUCTOS DESTACADOS</h2>
       <GallerySlider />
     </div>
   )
