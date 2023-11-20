cb9ee61b6010a8434042211cc5725eae11dc4b72
Daniel <carreraprogrammer@gmail.com>
2023-11-19 19:34:39 -0500

    Refactor Shop component

--- src/pages/Shop/Shop.js
+++
@@ -1,7 +0,0 @@
-const Shop = () => {
-    return(
-      <h1>This is the shop</h1>
-    )
-}
-
-export default Shop
---
+++ src/pages/Shop/Shop.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ShopHeader from '../../section/ShopHeader/ShopHeader'
+
+const Shop:React.FC = () => {
+  return (
+    <div>
+      <ShopHeader />
+    </div>
+  )
+}
+
+export default Shop
