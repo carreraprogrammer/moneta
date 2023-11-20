e38a342b51db130efdcff579924a0bd2fc15efe7
Daniel <carreraprogrammer@gmail.com>
2023-11-19 20:25:39 -0500

    Add FeaturedProducts component to Shop page

--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ShopHeader from '../../section/ShopHeader/ShopHeader'
+import FeaturedProducts from '../../section/FeaturedProducts/FeaturedProducts'
 
 const Shop:React.FC = () => {
   return (
     <div>
       <ShopHeader />
+      <FeaturedProducts />
     </div>
   )
 }
