7e5859aef38dbd1b21122ac1e06abdefd4461a1b
Daniel <carreraprogrammer@gmail.com>
2023-11-21 11:00:00 -0500

    Refactor ProductsGrid component and add scroll trigger animations

--- src/section/ProductsGrid/ProductsGrid.tsx
+++ src/section/ProductsGrid/ProductsGrid.tsx
@@ -1,20 +1,32 @@
-import React from 'react'
-import './ProductsGrid.scss'
-import { products, Product } from '../../assets/Information/Ecommerce'
-import { useMobileScreen } from '../../customHooks/useIsMobile'
+import React, { useRef, useEffect, RefObject } from 'react';
+import './ProductsGrid.scss';
+import { products, Product } from '../../assets/Information/Ecommerce';
+import { useMobileScreen } from '../../customHooks/useIsMobile';
+import useScrollTriggerAnimations from '../../customHooks/useScrollTriggerAnimations';
+import { fadeInAnimation } from '../../animations/animations';
 
 const ProductsGrid = () => {
-  const isMobile = useMobileScreen()
+  const isMobile = useMobileScreen();
+  const productsRefArray: React.RefObject<HTMLDivElement>[] = [];
 
+  products.forEach(() => {
+    const productRef = useRef<HTMLDivElement>(null);
+    productsRefArray.push(productRef);
+  });
+
+  productsRefArray.forEach((productRef: RefObject<HTMLDivElement>) => {
+    useScrollTriggerAnimations(productRef, [fadeInAnimation]);
+  });
+  
   return (
     <div className='productsGridContainer'>
-      {products.map((product:Product, index) => (
-        <div key={index} className='productContainer'>
+      {products.map((product: Product, index) => (
+        <div key={index} className='productContainer' ref={productsRefArray[index]}>
           <img src={isMobile ? product.imagenMovil : product.imagenPc} alt={product.nombre} className='productImage'/>
         </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default ProductsGrid
+export default ProductsGrid;
