ecc7ee85df01cbbb24f0119d02a444d148e12e76
Daniel <carreraprogrammer@gmail.com>
2023-11-20 20:11:31 -0500

    Update GallerySlider props in FeaturedProducts component

--- src/section/FeaturedProducts/FeaturedProducts.tsx
+++ src/section/FeaturedProducts/FeaturedProducts.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './FeaturedProducts.scss'
 import GallerySlider from '../../components/GallerySlider/GallerySlider'
+import { products } from  '../../assets/Information/Ecommerce'
+
 
 const FeaturedProducts = () => {
   return (
     <div className='featuredProductsContainer'>
       <h2 className='featuredProductsTitle'>PRODUCTOS DESTACADOS</h2>
-      <GallerySlider />
+      <GallerySlider products={products} />
     </div>
   )
 }
