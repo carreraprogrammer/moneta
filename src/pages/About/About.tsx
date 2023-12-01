e4a22a5c41d396d54f74ef006d0c950da5f3fe1d
Daniel <carreraprogrammer@gmail.com>
2023-11-30 19:23:20 -0500

    Update animations and add new sections

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import AboutHeader from '../../section/AboutHeader/AboutHeader'
+import AboutAbstract from '../../section/AboutAbstract/AboutAbstract'
+import Contact from '../../section/Contact/Contact'
+import Footer from '../../section/Footer/Footer'
 
 const About: React.FC = () => {
   return (
     <div>
       <AboutHeader />
+      <AboutAbstract />
+      <Contact />
+      <Footer />
     </div>
   )
 }
